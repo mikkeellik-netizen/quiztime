c866a1e20d749d7d533f9ef5828e8c6aaf44c3f0
mikkeellik-netizen <mikkeellik@gmail.com>
2026-05-26 02:16:59 +0300

    feat: register SpaController

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,8 +3,10 @@ import { PrismaModule } from './shared/prisma/prisma.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { QuizModule } from './modules/quiz/quiz.module';
 import { ImportModule } from './modules/import/import.module';
+import { SpaController } from './spa.controller';
 
 @Module({
   imports: [PrismaModule, AuthModule, QuizModule, ImportModule],
+  controllers: [SpaController],
 })
 export class AppModule {}
